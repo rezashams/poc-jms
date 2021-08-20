10511f4d3c244ec7e837ccbe192b1af1212906c0
Reza Shams <rezashams86@gmail.com>
2021-08-20 18:14:11 +0430

    first commit

---
+++ src/main/java/com/jms/PocJmsApplication.java
@@ -0,0 +1,13 @@
+package com.jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocJmsApplication.class, args);
+	}
+
+}
